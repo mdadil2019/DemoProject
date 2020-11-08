d51c980151015fe0f9c46d944289aff7271ddef7
MO RUSTAM <62928307+morustam818@users.noreply.github.com>
2020-11-08 17:31:56 +0530

    Update MainActivity.kt

--- app/src/main/java/com/proven/demoproject/MainActivity.kt
+++ app/src/main/java/com/proven/demoproject/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //Changes done by Rustam
+        //Changes done by Rustam - boss
     }
 }
